ab308873b57ed7703be3e0503c58c8553b11cd63
Zaidh Imran <zaidhimran2000@gmail.com>
2024-01-16 16:33:33 -0330

    checkout clean up

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -15,7 +15,7 @@ function CheckoutProduct({id, title, image, price, rating}) {
     }
     return (
         <div className='checkoutProduct'>
-            <img className='checkoutProduct__image' src={image} alt=''/>
+            <img className='checkoutProduct__image' src={image} alt={title}/>
             <div></div>
             <div className="checkoutProduct__info">
                 <p className="checkoutProduct__title">{title}</p>
